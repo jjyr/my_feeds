5653216b5720c8c0a382a9cb99f11650e73a19b0
jjy <jjyruby@gmail.com>
2013-05-11 14:56:08 +0800

    add templates/model

---
+++ lib/my_feeds/generators/templates/model.rb
@@ -0,0 +1,9 @@
+class Feed < ActiveRecord::Base
+  attr_accessible :source_id, :source_type, :event, :target_id, :target_type
+
+  validates :event, uniqueness: {scope: [:source_id, :source_type, :target_id, :target_type]}, :if => :target_id
+
+  belongs_to :source, polymorphic: true
+
+  belongs_to :target, polymorphic: true
+end
