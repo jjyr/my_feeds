f06e3e8390fde129fe9170893147319a1c6dbad2
jjy <jjyruby@gmail.com>
2013-05-11 15:43:54 +0800

    fix NotImplementedError

--- lib/rails/generators/my_feeds_generator.rb
+++ lib/rails/generators/my_feeds_generator.rb
@@ -12,6 +12,14 @@ module MyFeeds
         migration_template 'migration.rb', 'db/migrate/my_feeds_migration.rb'
       end
 
+      def self.next_migration_number(dirname)
+        if ActiveRecord::Base.timestamped_migrations
+          Time.now.utc.strftime("%Y%m%d%H%M%S")
+        else
+          "%.3d" % (current_migration_number(dirname) + 1)
+        end
+      end
+
       def create_model
         template "model.rb", File.join('app/models', "feed.rb")
       end
