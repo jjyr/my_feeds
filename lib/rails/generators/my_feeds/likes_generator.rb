68e284ddc990eb24c8a12eab1ca12e4207f69e64
jjy <jjyruby@gmail.com>
2013-05-11 18:45:58 +0800

    add generator scope

---
+++ lib/rails/generators/my_feeds/likes_generator.rb
@@ -0,0 +1,29 @@
+module MyFeeds
+  module Generators
+    class LikesGenerator < Rails::Generators::Base
+
+      include Rails::Generators::Migration
+
+      def self.source_root
+        @source_root ||= File.join(File.dirname(__FILE__), 'templates')
+      end
+
+      def create_migration_file
+        migration_template 'migration.rb', 'db/migrate/my_feeds_migration.rb'
+      end
+
+      def self.next_migration_number(dirname)
+        if ActiveRecord::Base.timestamped_migrations
+          Time.now.utc.strftime("%Y%m%d%H%M%S")
+        else
+          "%.3d" % (current_migration_number(dirname) + 1)
+        end
+      end
+
+      def create_model
+        template "model.rb", File.join('app/models', "feed.rb")
+      end
+    end
+  end
+end
+
