d8187832c3e120216198b6f6f937237c0d520a4b
jjy <jjyruby@gmail.com>
2013-05-11 15:53:38 +0800

    add a comment

--- lib/rails/generators/templates/model.rb
+++ lib/rails/generators/templates/model.rb
@@ -1,6 +1,7 @@
 class Feed < ActiveRecord::Base
   attr_accessible :source_id, :source_type, :event, :target_id, :target_type
 
+  #verify event uniqueness, you can remove this
   validates :event, uniqueness: {scope: [:source_id, :source_type, :target_id, :target_type]}, :if => :target_id
 
   belongs_to :source, polymorphic: true
