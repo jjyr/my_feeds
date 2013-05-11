064d9ad5ec3ec5302ab54e023362f7aaaa0d728c
jjy <jjyruby@gmail.com>
2013-05-11 14:59:09 +0800

    add feeder

---
+++ lib/my_feeds/feeder.rb
@@ -0,0 +1,23 @@
+module MyFeeds
+  module Feeder
+    extend ActiveSupport::Concern
+
+    included do
+      has_many :source_feeds, class_name: Feed.to_s, as: :source, dependent: :destroy
+    end
+
+    module ClassMethods
+      def define_feed_event event, conditions = {}
+        conditions.freeze
+        define_method(event) do |options = {}|
+          attributes = {source_type: self.class.to_s, source_id: self.id, event: event}.merge!(conditions).merge! options
+        Feed.create attributes
+        end
+
+        define_method("#{event}_feeds") do
+          source_feeds.where({event: event}.merge! conditions)
+        end
+      end
+    end
+  end
+end
