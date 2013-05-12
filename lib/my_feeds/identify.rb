87843fd5607932209ef1adef06247f5a62b6e99e
jjy <jjyruby@gmail.com>
2013-05-12 21:24:05 +0800

    better way, maybe..

--- lib/my_feeds/identify.rb
+++ lib/my_feeds/identify.rb
@@ -4,18 +4,25 @@ module MyFeeds
 
     included do
       before_save :save_feed_identify
-      class_attribute :feed_polymorphic_name
       self.feed_polymorphic_name = self == Feed ? :source : default_feed_polymorphic_name
       delegate :default_feed_polymorphic_name, :polymorphic_identify_column, :polymorphic_id_column, :polymorphic_type_column, to: :"self.class"
     end
 
-    protected
-    def save_feed_identify
-      instance_eval %Q{self.#{polymorphic_identify_column} = "#\{#{polymorphic_id_column}.to_s\}@#\{#{polymorphic_type_column}.to_s.underscore\}"}
-    end
-
-    private
     module ClassMethods
+      def feed_polymorphic_name
+        @_feed_polymorphic_name
+      end
+
+      def feed_polymorphic_name= polymorphic_name
+        @_feed_polymorphic_name = polymorphic_name
+        class_eval %Q{
+          def save_feed_identify
+            self.#{polymorphic_identify_column} = "#\{#{polymorphic_id_column}.to_s\}@#\{#{polymorphic_type_column}.to_s.underscore\}"
+          end
+          protected :save_feed_identify
+        }
+      end
+
       def default_feed_polymorphic_name
         self.to_s.underscore
       end
